0d43933beb5a1c2f0031d4b403e5d4cdb401884b
Leandro Gonçalves <leandrodebritogoncalves@gmail.com>
2021-05-05 18:02:54 -0400

    finalizando a preparaçao do ambiente instalaçao do sass

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,7 @@
-import React from 'react';
+//importo do react DOM o que vai ser renderizado no meu html
+
+import { render } from 'react-dom';
 import { App } from './App'
+
+//aqui eu chamo a função render e no parametro infor dentro de qual elemento eu quero exibir em tela e dentro de qual ID
+render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require( 'path' )
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+//Configuração para que o Webpack consiga identificar se esta em ambiente de desenvolvimento ou produção
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -9,12 +15,25 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ],
     }
